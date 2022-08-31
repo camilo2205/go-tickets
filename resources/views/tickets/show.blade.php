80a6c510c1111e1927e7ea11a182246aace5f0a1
Cristian Ledesma <ledesmalopezcristian@gmail.com>
2022-08-31 16:06:16 -0500

    Mostrar fecha del ticket

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -6,9 +6,9 @@
                     / <span class="font-bold">Ver</span>
                 </span>
             </div>
-            <div class="basis-1/3 self-end">
+            <div class="basis-2/3 self-end">
                 <h2 class="font-semibold text-2xl text-gray-800 leading-tight">
-                    {{ __('TICKET') }} #{{ str_pad($ticket->id, 5, '0', STR_PAD_LEFT) }}
+                    {{ __('TICKET') }} #{{ str_pad($ticket->id, 5, '0', STR_PAD_LEFT) }} ({{ formatDate($ticket->created_at)}})
                 </h2>
             </div>
             @if (session('error'))
