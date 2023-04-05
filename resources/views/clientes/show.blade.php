3c1b2461501e615b2a63e24fb52bd046b2166ee8
Fernando Jose Haylock Aguilar <ferhaylock15@outlook.es>
2023-04-05 16:19:57 -0500

    Correcion fecha

--- resources/views/clientes/show.blade.php
+++ resources/views/clientes/show.blade.php
@@ -80,7 +80,6 @@
                                     <li
                                     class="inline-flex items-center gap-x-2 py-3 px-4 text-sm font-medium bg-white border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg dark:bg-gray-800 dark:border-gray-700 dark:text-white">
                                     <div class="flex items-center">
-                                        {{$disk->created_at}}
                                         <input id="default-checkbox" type="checkbox"  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" data-id="{{$disk->id}}"  {{$disk->notificable === true ? "checked" :' '}}>
                                         <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Notificable</label>
                                     </div>
