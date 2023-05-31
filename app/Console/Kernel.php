70874305639aa5a6efb8b8c6c8300616dc82c861
Cristian Camilo Ledesma López <ledesmalopezcristian@gmail.com>
2023-05-31 11:04:07 -0500

    Notificar espacio en servidor cada media hora

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('get:token')->everySixHours();
         $schedule->command('send:webpush')->everyMinute();
         $schedule->command('send:webpushcliente')->everyMinute();
-        $schedule->command('send:webpushserver')->everyTwoMinutes();
+        $schedule->command('send:webpushserver')->everyThirtyMinutes();
     }
 
     /**
