402484bb8772c97dfd119dbab4eac1a6b3300d43
Fernando Haylock <ferhaylock15@gmail.com>
2024-12-03 15:59:22 -0500

    fix: agrega campos en archivo de app

--- config/app.php
+++ config/app.php
@@ -35,7 +35,7 @@ return [
 
     'ws_host' => env('WS_HOST', 'localhost'),  // Dirección de tu servidor WebSocket
     'ws_port' => env('WS_PORT', 6001), 
-    'wss_port' => env('WSS_PORT', 443),
+    'wss_port' => env('WSS_PORT', 6001),
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
